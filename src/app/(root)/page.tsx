223f9b13c86c971a47d3f84db9bbc1170d5eda9a
AG <ag@MacBook-Pro.local>
2024-02-28 15:17:55 -0500

    initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+    
+    </div>
+  );
+};
+
+export default HomePage;
+ 
